402fecb578237aadab5ad38934eddf0f1d914685
Bagas-png <70693513+Bagas-png@users.noreply.github.com>
2021-12-29 15:15:55 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector("#emoji");
+
+const emojis = ["🤣", "😝", "😍", "😶", "🙂", "😵", "🤐", "😜", "😋", "😃"];
+
+btn.addEventListener("mouseover", () => {
+  btn.innerText = emojis[Math.floor(Math.random() * emojis.length)];
+});
